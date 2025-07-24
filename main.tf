108ff150d130a089e7eef3468e62c60e1ff83e4c
aimakeratwill <aimakeratwill@gmail.com>
2025-07-24 14:52:47 +0000

    changes

--- main.tf
+++ main.tf
@@ -79,7 +79,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                = "ollama-vm"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_B2s"
+  size                = "Standard_D4s_v3"
   admin_username      = "azureuser"
   network_interface_ids = [
     azurerm_network_interface.nic.id,

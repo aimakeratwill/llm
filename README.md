d6aff40afb3502fe4b8b03d73245804271cba72a
aimakeratwill <aimakeratwill@gmail.com>
2025-07-24 04:08:30 +0000

    ollama

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# llm
+# Ollama Azure VM Deployment (Terraform)
+
+This repo sets up an Azure VM to run Ollama with multiple LLMs using Terraform and cloud-init.
+
+## 🚀 Steps to Deploy
+
+1. Clone this repo
+2. Open it in GitHub Codespaces or locally
+3. Run:
+
+```bash
+az login
+ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ""
+terraform init
+terraform apply
+```
+
+4. SSH into your VM:
+
+```bash
+ssh azureuser@<your-public-ip>
+```
+
+Ollama will be running at port `11434`.
---
+++ main.tf
@@ -0,0 +1,76 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "ollama-rg"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "ollama-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "ollama-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = "ollama-pip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "ollama-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "ollama-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B2s"
+  admin_username      = "azureuser"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+
+  custom_data = filebase64("cloud-init.txt")
+
+  tags = {
+    environment = "Ollama"
+  }
+}
